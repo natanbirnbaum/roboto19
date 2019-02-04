7c6b4f3aaca2f2ac5141faf3b1b172853be91c5c
Natan Birnbaum <natan.birnbaum@bcs448.org>
2019-02-04 18:27:30 -0500

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,11 +10,17 @@ import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.CameraServer;
+// import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
+// Camera imports
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.cscore.VideoSink;
+import edu.wpi.cscore.VideoSource.ConnectionStrategy;
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.networktables.NetworkTableInstance;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -33,6 +39,11 @@ public class Robot extends TimedRobot {
 	Spark rightmotor = new Spark(1);
 	Joystick controller = new Joystick(0);
 	DifferentialDrive driver = new DifferentialDrive(leftmotor, rightmotor);
+
+	UsbCamera camera1;
+	UsbCamera camera2;
+	VideoSink server;
+	boolean camera1Selected;
 	
 	/**
 	 * This function is run when the robot is first started up and should be
@@ -43,6 +54,15 @@ public class Robot extends TimedRobot {
 		m_chooser.addDefault("Default Auto", kDefaultAuto);		 
 		m_chooser.addObject("My Auto", kCustomAuto);
 		SmartDashboard.putData("Auto choices", m_chooser);
+
+		camera1 = CameraServer.getInstance().startAutomaticCapture(0);
+		// camera2 = CameraServer.getInstance().startAutomaticCapture(1);
+		server = CameraServer.getInstance().getServer();
+
+		camera1.setConnectionStrategy(ConnectionStrategy.kKeepOpen);
+		// camera2.setConnectionStrategy(ConnectionStrategy.kKeepOpen);
+		camera1Selected = true;
+		server.setSource(camera1);
 	}
 	
 	/**
@@ -92,17 +112,27 @@ public class Robot extends TimedRobot {
 		double xAxis = controller.getRawAxis(0);
 		double yAxis = controller.getRawAxis(1);
 
-	  boolean JoystickButton = controller.getRawButton(5);
-
-		if (!JoystickButton){
-		forwardSpeed = yAxis;
-		rotationSpeed = xAxis;
+	  	boolean precisionButton = controller.getRawButton(6);
+		  
+		if (!precisionButton){
+			forwardSpeed = yAxis;
+			rotationSpeed = xAxis;
 		} else{
-		forwardSpeed = yAxis*0.7;
-		rotationSpeed = xAxis*0.5;
+			forwardSpeed = yAxis*0.7;
+			rotationSpeed = xAxis*0.5;
 		}
-
 		driver.arcadeDrive(forwardSpeed, rotationSpeed, true);
+
 		
-				}
+		// camera button
+		boolean cameraButton = controller.getRawButton(1);
+		if (camera1Selected) {
+			server.setSource(camera2);
+			camera1Selected = false;
+		} else {
+			server.setSource(camera1);
+			camera1Selected = true;
 		}
+		
+	}
+}
